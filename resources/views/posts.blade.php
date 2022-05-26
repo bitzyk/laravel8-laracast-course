14e572e1bfbe228696685814647f99b92c6fbff5
Cristian Bitoi <cbitoi@radial.com>
2022-05-26 19:44:41 +0300

    laravel db - route-model binding

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,11 +2,14 @@
     @foreach($posts as $post)
         <article class="{{ $loop->even ? 'even' : '' }}">
             <h1>
-                <a href="/post/{{ $post->id }}">
+                <a href="/post/{{ $post->slug }}">
                     {{ $post->title }}
                 </a>
             </h1>
             <p>{{ $post->excerpt }}</p>
+            <div>
+                <a href="/post-by-id/{{ $post->id }}">post route by id</a>
+            </div>
         </article>
     @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,21 @@ Route::get('/', function () {
 });
 
 /**
- * route model binding -> {post} should be the same with \App\Models\Post
+ * route model binding by a field that is not id -> {post} should be the same with $post
  */
-Route::get('post/{post}', function(\App\Models\Post $post) {
+Route::get('post/{post:slug}', function(\App\Models\Post $post) {
     // find a post by its slug and pass it to a view called "post"
     return view('post', [
         'post' => $post
     ]);
-})->where('postSlug', '[A-z1-9\-]+');
+})->where('post', '[A-z\-]+');
+
+/**
+ * route model binding -> {post} should be the same with $post
+ */
+Route::get('post-by-id/{post}', function(\App\Models\Post $post) {
+    // find a post by its slug and pass it to a view called "post"
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[1-9]+');
